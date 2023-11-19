530d11d704cf3a0da645a4c1d31a222a535f3e9f
Vishal Mahajan <mahajanvishal410a@gmail.com>
2023-11-20 00:15:59 +0530

    changes

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,5 +1,23 @@
 package Sorting;
 
 public class MergeSort {
-
+	
+	static int stackFrame=0;
+	
+	public static void divide(int[] arr, int start, int end) {
+		stackFrame++;
+		if(start<end) {
+			int mid = start + (end-start)/2;
+			System.out.println("Start : "+start+" Mid : "+mid+" End : "+end);
+			divide(arr,start,mid);
+			divide(arr,mid+1,end);
+		}
+	}
+	
+	public static void main(String[] args) {
+		stackFrame++;
+		int[] arr = new int[] {8,3,5,4,1,6,9,2};
+		divide(arr, 0, arr.length-1);
+		System.out.println(stackFrame);
+	}
 }
