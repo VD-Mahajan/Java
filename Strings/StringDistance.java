87b80638ab7c24689b71f52616fd3c11c228b25c
Vishal Mahajan <mahajanvishal410a@gmail.com>
2023-12-30 18:01:57 +0530

    Initial commit

--- Strings/StringDistance.java
+++ Strings/StringDistance.java
@@ -4,11 +4,11 @@ public class StringDistance {
 	public static void main(String[] args) {
 		String s1 = "Vishal";
 		String s2 = "Kedar";
-		
+
 		String[] arr = new String[] {"Kedar","Kapse","Shiradhon","Sumbulung","Vishal","Mahajan"};
 		int count=0;
 		boolean flag=false;
-		
+
 		for(int i=0;i<arr.length;i++) {
 			if((arr[i].equals(s1) || arr[i].equals(s2)) && count==0) {
 				flag =true;
@@ -16,7 +16,7 @@ public class StringDistance {
 			}
 			if(flag==true)
 				count++;
-			
+
 			if((arr[i].equals(s2) || arr[i].equals(s1)) && flag == true) {
 				System.out.println("Distance is "+count);
 			}
