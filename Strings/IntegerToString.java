8eee30eee3d37a21c8718512e862255f3799450e
Vishal Mahajan <mahajanvishal410a@gmail.com>
2023-12-31 12:40:07 +0530

    Initial commit

---
+++ Strings/IntegerToString.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class IntegerToString {
+	
+	public static void main(String[] args) {
+		int i = 119;
+		StringBuffer sb = new StringBuffer(i);
+		System.out.println(sb);
+	}
+}
