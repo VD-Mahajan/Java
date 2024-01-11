4468fc1874a58671356c58634507b941240f0a16
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-11 19:54:01 +0530

    Initial commit

---
+++ Pattern/HollowReverseTrianglePattern.java
@@ -0,0 +1,23 @@
+package Pattern;
+
+public class HollowReverseTrianglePattern {
+	static void printPattern(int x) {
+		for(int i=x;i>=1;i--) {
+			for(int j=1;j<=x-i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=i;j>=1;j--) {
+//				System.out.print("* ");
+				if(j==i || i==x || j==1)
+					System.out.print("* ");
+				else
+					System.out.print("  ");					
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		printPattern(5);
+	}
+}
