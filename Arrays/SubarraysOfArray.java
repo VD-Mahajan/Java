6144a866bf386574bee4775e142e8043eaa6338c
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-02-25 23:27:10 +0530

    Initial commit

---
+++ Arrays/SubarraysOfArray.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class SubarraysOfArray {
+	public static void main(String[] args) {
+		int[] arr = new int[] {1,2,3,4,5};
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				for (int k = i; k <j; k++) {
+					System.out.print(arr[k]+" ");					
+				}
+				System.out.println();
+			}
+//			System.out.println();
+		}
+	}
+}	
