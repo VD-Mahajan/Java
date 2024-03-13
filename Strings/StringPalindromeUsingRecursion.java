7cd2eb35d7ba6794105c511959e48eab2188c732
Vishal Datta Mahajan <mahajanvishal410a@gmail.com>
2024-03-13 15:37:00 +0530

    new commit

--- Strings/StringPalindromeUsingRecursion.java
+++ Strings/StringPalindromeUsingRecursion.java
@@ -2,4 +2,20 @@ package Strings;
 
 public class StringPalindromeUsingRecursion {
 
+	static boolean isPalindrome(String str , int start,int end) {
+		if(start>=end) {
+			return true;
+		}
+		if(str.charAt(start)!=str.charAt(end)) {
+			return false;
+		}else {
+			return isPalindrome(str, start+1, end-1);
+		}
+//		return true;
+	}
+
+	public static void main(String[] args) {
+		String str = "mkm";
+		System.out.println(isPalindrome(str, 0, str.length()-1));
+	}
 }
